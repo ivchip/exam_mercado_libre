d2d41d8e6ffd3f1b711645d83d94267cb5dc1d50
Ivan Chipatecua <ivchip@gmail.com>
2021-02-08 00:15:39 -0500

    Initial project mlme

---
+++ src/main/java/com/idch/mlme/MercadoLibreMutantExamApplication.java
@@ -0,0 +1,13 @@
+package com.idch.mlme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLibreMutantExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoLibreMutantExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idch/mlme/MercadoLibreMutantExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idch.mlme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLibreMutantExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
